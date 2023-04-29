c2e8e97a7f001f108a742e613b989ba15d3f6c1a
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2023-04-29 17:47:44 +0100

    GameEngine files updated

--- TinyPAc/src/pt/isec/pa/tinypac/gameengine/GameEngine.java
+++ TinyPAc/src/pt/isec/pa/tinypac/gameengine/GameEngine.java
@@ -1,14 +1,20 @@
 package pt.isec.pa.tinypac.gameengine;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
 public final class GameEngine implements IGameEngine {
     private GameEngineState state;
     private GameEngineThread controlThread;
     private Set<IGameEngineEvolve> clients;
     System.Logger logger;
+
     private void setState(GameEngineState state) {
-        this.state = state; logger.log(System.Logger.Level.INFO,state.toString());
+        this.state = state;
+        logger.log(System.Logger.Level.INFO,state.toString());
     }
+
     public GameEngine() {
         logger = System.getLogger("GameEngine");
         clients = new HashSet<>();
@@ -19,10 +25,12 @@ public final class GameEngine implements IGameEngine {
     public void registerClient(IGameEngineEvolve listener) {
         clients.add(listener);
     }
+
     @Override
     public void unregisterClient(IGameEngineEvolve listener) {
         clients.remove(listener);
     }
+
     @Override
     public boolean start(long interval) {
         if (state != GameEngineState.READY)
@@ -30,8 +38,9 @@ public final class GameEngine implements IGameEngine {
         controlThread = new GameEngineThread(interval);
         setState(GameEngineState.RUNNING);
         controlThread.start();
-        return false;
+        return true;
     }
+
     @Override
     public boolean stop() {
         if (state == GameEngineState.READY)
@@ -39,6 +48,7 @@ public final class GameEngine implements IGameEngine {
         setState(GameEngineState.READY);
         return true;
     }
+
     @Override
     public boolean pause() {
         if (state != GameEngineState.RUNNING)
@@ -46,34 +56,43 @@ public final class GameEngine implements IGameEngine {
         setState(GameEngineState.PAUSED);
         return true;
     }
+
     @Override
     public boolean resume() {
         if (state != GameEngineState.PAUSED)
             return false;
         setState(GameEngineState.RUNNING);
-        return false;
+        return true;
     }
+
     @Override
     public GameEngineState getCurrentState() {
         return state;
     }
+
     @Override
     public long getInterval() {
         return controlThread.interval;
     }
+
     @Override
     public void setInterval(long newInterval) {
-        if (controlThread != null) controlThread.interval = newInterval;
+        if (controlThread != null)
+            controlThread.interval = newInterval;
     }
 
     @Override
     public void waitForTheEnd() {
+        //controlThread.setDaemon(false);
         try {
             controlThread.join();
-        } catch (InterruptedException e) {}
+        } catch (InterruptedException e) {
+        }
     }
+
     private class GameEngineThread extends Thread {
         long interval;
+
         GameEngineThread(long interval) {
             this.interval = interval;
             this.setDaemon(true);
@@ -82,10 +101,12 @@ public final class GameEngine implements IGameEngine {
         public void run() {
             int errCounter = 0;
             while (true) {
-                if (state == GameEngineState.READY) break; if (state == GameEngineState.RUNNING) {
+                if (state == GameEngineState.READY)
+                    break;
+                if (state == GameEngineState.RUNNING) {
                     new Thread(() -> {
-                        long time = System.nanoTime(); clients.forEach(
-                                client -> client.evolve(GameEngine.this, time) );
+                        long time = System.nanoTime();
+                        List.copyOf(clients).forEach(client -> client.evolve(GameEngine.this, time));
                     }).start();
                 }
                 try {
@@ -97,7 +118,6 @@ public final class GameEngine implements IGameEngine {
                         break;
                 }
             }
-
         }
     }
 }
